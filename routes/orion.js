2f39671df4d5046c6e4b35c9c565a6045d13c2fd
Colin <colin.beauvisage@gmail.com>
2021-05-30 21:52:57 +0200

    wip

--- routes/orion.js
+++ routes/orion.js
@@ -8,15 +8,6 @@ const putAll = async (req, res) => {
     try {
         await insertAllEntities(await getJcdecaux());
         await insertAllEntities(await getOpenData());
-        // const a = await getOpenData();
-        //
-        // const b = [];
-        // for (let i = 0 ; i < 200; i++) b.push(a[i]);
-        //
-        // console.log(b[68]);
-        // console.log(b[69]);
-        // console.log(b[70]);
-        // await insertAllEntities(b);
 
         res.send();
     } catch(err) {
